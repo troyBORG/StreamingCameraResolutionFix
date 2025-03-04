f55c30a4d81be019b37fc6294fffbffe2a09cc1a
troyBORG <troyborg@gmail.com>
2025-03-04 14:12:26 -0600

    Create AssemblyInfo.cs

---
+++ StreamingCameraResolutionFix/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("StreamingCameraResolutionFix")]
+[assembly: AssemblyProduct("StreamingCameraResolutionFix")]
+[assembly: AssemblyDescription("Fixes Streaming Camera to not be 16x9")]
+[assembly: AssemblyCompany("troyBORG")]
+[assembly: AssemblyCopyright("Copyright © 2025 troyBORG")]
+[assembly: AssemblyVersion(StreamingCameraResolutionFix.StreamingCameraResolutionFix.VERSION_CONSTANT)]
+[assembly: AssemblyFileVersion(StreamingCameraResolutionFix.StreamingCameraResolutionFix.VERSION_CONSTANT)]
